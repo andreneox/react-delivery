b99dc17bde767017bdf6a411caa49ea82a53d512
unknown <giiozera24@gmail.com>
2023-01-25 15:08:49 -0400

    Responsividade card

--- src/pages/home/HomeAdmin.jsx
+++ src/pages/home/HomeAdmin.jsx
@@ -7,7 +7,7 @@ import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
-import {useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import { AppBarAdmin } from "../../components/appbar/AppBarAdmin"
 import { api } from "../../config/Api"
 import { ValidaLogin } from "../../config/ValidaLogin"
@@ -20,20 +20,20 @@ import Box from '@mui/material/Box';
 export const HomeAdmin = () => {
   const [url, setUrl] = useState('http://localhost:3005/files/')
   const [cardapio, setCardapio] = useState([])
-  
+
 
 
   useEffect(() => {
-    api.get('/Cardapio',{
+    api.get('/Cardapio', {
       headers: {
         "authorization": localStorage.getItem('token')
-    }
+      }
     })
       .then(function (response) {
         // manipula o sucesso da requisição
-       
+
         setCardapio(response.data.data)
-     
+
       })
       .catch(function (error) {
         // manipula erros da requisição
@@ -45,41 +45,41 @@ export const HomeAdmin = () => {
 
 
   return (
-<ValidaLogin>
- <AppBarAdmin/>
-     <Container>
-      <Grid container sx={{mt:'70px'}} spacing={2}>
-        {cardapio.map((cardapios,index)=>(
-      <Grid  key={index} item xl={3} sm={4} lg={4} md={4} xs={6}>
-      <Card sx={{display:'flex',flexDirection:'column',alignItems:'center',maxwidth:'350px',height:'300px'}}>
-        <Box sx={{width:'90%',height:'450px'}}>
+    <ValidaLogin>
+      <AppBarAdmin />
+      <Container>
+        <Grid container sx={{ mt: '70px' }} spacing={2}>
+          {cardapio.map((cardapios, index) => (
+            <Grid key={index} item xl={3} sm={4} lg={4} md={4} xs={6}>
+              <Card sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', maxwidth: '350px', height: '300px' }}>
+                <Box sx={{ width: '90%', height: '450px' }}>
 
-      <CardMedia
-      sx={{width:'100%',height:'100%',borderRadius:1}}
-     image={url + cardapios.img}
-      >
-      </CardMedia>
-      </Box>
-      <CardContent>
-      <Typography gutterBottom variant="h5" component="div">
-        {cardapios.nome}
-        </Typography>
-        <Typography  variant="h5" component="div">
-        {cardapios.nome}
-        </Typography>
-      </CardContent>
-      <Box sx={{display:'flex',justifyContent:'center',alignItems:'center',width:'100%',height:'100%'}}>
-      <CardActions sx={{display:'flex',flexWrap:'wrap',justifyContent:'center',width:'100%'}}>
-      <Button size="small">Remover</Button>
-      <Button size="small">Editar</Button>
-      <Button size="small">Pausar</Button>
-     
-      </CardActions>
-      </Box>
-      </Card>
-      </Grid>
-             ))}
-      </Grid>
+                  <CardMedia
+                    sx={{ width: '100%', height: '100%', borderRadius: 1 }}
+                    image={url + cardapios.img}
+                  >
+                  </CardMedia>
+                </Box>
+                <CardContent sx={{display:'flex',flexDirection:'column',alignItems:'center'}}>
+                  <Typography gutterBottom variant="h8" component="div">
+                    {cardapios.nome}
+                  </Typography>
+                  <Typography variant="h8" fontWeight='bold' component="div">
+                    {cardapios.valor}R$
+                  </Typography>
+                </CardContent>
+                <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}>
+                  <CardActions sx={{ display:{ xl:'flex', flexWrap: 'nowrap', justifyContent: 'center', width: '100%'}}}>
+                    <Button variant="contained" size="small">Remover</Button>
+                    <Button variant="contained" size="small">Editar</Button>
+                    <Button variant="contained" size="small">Pausar</Button>
+
+                  </CardActions>
+                </Box>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </Container>
     </ValidaLogin>
   )
